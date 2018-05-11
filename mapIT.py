49660f6926d10c4abb99965e10a08cdfcb34d745
Jokertion <645434934@qq.com>
2018-05-11 17:59:57 +0800

    自动加载地图

---
+++ mapIT.py
@@ -0,0 +1,16 @@
+#! python3  复制地址自动google map查询
+# mapIT.py - Launches a map in the browser using an address from the 
+# command line or clipboard.
+#技能包：webbrowser模块
+
+import webbrowser, sys, pyperclip
+if len(sys.argv) > 1:
+	#Get address from command line.
+	address = ' '.join(sys.argv[1:])
+else:
+	#Get address from clipboard.
+	address = pyperclip.paste()
+	
+webbrowser.open('https://www.google.com/maps/place/' + address)
+
+	

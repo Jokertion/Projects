a90c50e8a416e3be15e34942e953c08bbc1a0018
Jokertion <645434934@qq.com>
2018-05-11 15:52:01 +0800

    把粘贴板的内容,每一行前面添加星号

---
+++ bulletPointAdder.py
@@ -0,0 +1,16 @@
+#! python3  #把粘贴板的内容每一行前面添加星号
+# bulletPoinAdder.py - Adds Wikipedia bullet points to the start
+# of each line of text on the clipboard.
+#技能包： 1.pyperclip 2.列表的split<-->join 方法
+import pyperclip
+text = pyperclip.paste()
+#TODO:Separate lines and add stars.
+
+#Separate lines and add stars.
+lines = text.split('\n')  #按换行分割文本，得到列表
+for i in range(len(lines)):  #列表每个项是文本中的一行
+	lines[i] = '* ' + lines[i]
+text = '\n'.join(lines)  #还原回去
+pyperclip.copy(text)
+print('Has been added, it can be directly pasted.')
+	

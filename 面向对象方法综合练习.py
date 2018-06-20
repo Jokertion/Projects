96d9bbff4282fab32b70a0ad29a359c7be8f05be
Miracle_lms <37183438+Jokertion@users.noreply.github.com>
2018-06-20 23:41:25 +0800

    面向对象方法综合练习.py

--- 面向对象方法综合练习.py
+++ 面向对象方法综合练习.py
@@ -6,17 +6,17 @@ class Game(object):
 	def __init__(self,player_name):
 		self.player_name = player_name
 		
-	#定义静态方法
+	#定义 静态方法
 	@staticmethod
 	def show_help():
 		print("帮助信息：让僵尸进入大门")
 		
-	#定义类方法	
+	#定义 类方法	
 	@classmethod
 	def show_top_score(cls):
 		print("历史记录: %d分 "% cls.top_score)
 		
-	#定义实例方法
+	#定义 实例方法
 	def start_game(self):
 		print("%s 游戏开始啦..."% self.player_name)
 
@@ -26,6 +26,6 @@ Game.show_help()        #调用静态方法
 #2.查看历史最高分
 Game.show_top_score()   #调用类方法
 
-#3.创建游戏对象         #调用实例方法
+#3.创建游戏对象          #调用实例方法
 game = Game("小明")
 game.start_game()

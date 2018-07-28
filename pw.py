3e2ffcd3cea9740a263623f5d7beb082e8dda39a
Miracle_lms <37183438+Jokertion@users.noreply.github.com>
2018-07-29 00:33:21 +0800

    口令保管箱
    项目：口令保管箱 
    你可能在许多不同网站上拥有账号， 每个账号使用相同的口令是个坏习惯。如果这些网站中任何一个有安全漏洞， 黑客就会知道你所有的其他账号的口令。最好是在你的计算机上， 使用口令管理器软件， 利用一个主控口令， 解锁口令管理器。然后将某个账户口令拷贝到剪贴板， 再将它粘贴到网站的口令输入框。
    
    目的：用于保存各种账号的密码，输入账户后会将账户密码拷贝到剪贴板，可直接粘贴到网站的密码输入框 
    
    使用说明：
    
    1.代码部分
    
    2.建立bat批处理文件，输入内容：
    @python.exe C:\pw.py %*
    @pause
    保存为pw.bat文件，和pw.py放一块 
    保存在‘C:\’中
    
    3、设置环境变量，将bat路径加入Path变量中
    
    4、win + R 打开运行窗口，输入 “pw QQ”,
    跳出提示窗口，QQ密码已经复制到剪贴板上
    
    粘贴内容：
    12345

--- pw.py
+++ pw.py
@@ -1,38 +1,62 @@
-#! python3 
-#pw.py - An insure password locker program.
-#path：C:\
-#技能包：1.建立bat批处理文件 2.pyperclip
+#! python3
+# pw.py - An insure password locker program.
+# path：C:\
+# 技能包：1.建立bat批处理文件 2.pyperclip
 
-PASSWORDS= {'sina':'f7MinlBDDuvmKJBbjshbsaccjJAksgnbbf8',
-			'baidu':'gKCXkiajJJnvbCdDxfrgHujikGFEeHJMjmi8',
-			'QQ':'12345'
-			}
-		
-import sys,pyperclip
-if len(sys.argv) < 2: #如果命令行输入参数少于两个，显示用法信息
-	print('Usage: python pw.py [account] = copy account password')
-	sys.exit()
-	
-account = sys.argv[1] #first command line arg is the account name
+import sys, pyperclip
 
-#字典中查找账户名，如果名称是字典中的键，取得其value，将它复制到剪贴板，然后打印一条信息；否则，告知没有这个账户名
-if account in PASSWORDS: 
-	pyperclip.copy(PASSWORDS[account])
-	print ('Password for ' + account + ' copied to clipboard.')
-else:
-	print('There is no account named' + account)
-	
+
+def password():
+    PASSWORDS = {
+        'sina': 'f7MinlBDDuvmKJBbjshbsaccjJAksgnbbf8',
+        'baidu': 'gKCXkiajJJnvbCdDxfrgHujikGFEeHJMjmi8',
+        'QQ': '12345',
+    }
+    return PASSWORDS
+
+
+def judge_input():
+    # 如果命令行输入参数少于两个，提示用法信息
+    if len(sys.argv) < 2:
+        print('Usage: python pw.py [account] = copy account password')
+        sys.exit()
+
+    account = sys.argv[1]
+    code(account)
+
+
+def code(account):
+    PASSWORDS = password()
+    # 字典中查找账户名，
+    # 如果,是字典中的键，取得其value，
+    # 复制到剪贴板，然后打印提示信息；
+    # 否则，告知没有这个账户名
+    if account in PASSWORDS:
+        pyperclip.copy(PASSWORDS[account])
+        print('Password for ' + account + ' copied to clipboard.')
+    else:
+        print('There is no account named' + account)
+
+
+if __name__ == '__main__':
+    judge_input()
+
 '''
+使用说明：
+
 1.代码部分
-2.将代码保存，以管理员身份运行记事本，建立一个bat批处理文件，内容：
 
-@python.exe C:\pw.py %* 
+2.建立bat批处理文件，输入内容：
+@python.exe C:\pw.py %*
 @pause
-保存为pw.bat文件，保存在‘C:\’中 
-3、设置环境变量，将bat文件所在路径加入Path变量中
+保存为pw.bat文件，和pw.py放一块 
+保存在‘C:\’中
 
-4、win + R 打开运行窗口，输入 “pw QQ”,点击“确定”，会跳出提示窗口，并将密码复制到剪贴板上
+3、设置环境变量，将bat路径加入Path变量中
 
-右键粘贴内容： 
+4、win + R 打开运行窗口，输入 “pw QQ”,
+跳出提示窗口，QQ密码已经复制到剪贴板上
+
+粘贴内容：
 12345
 '''
